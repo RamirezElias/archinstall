92bc558cffd9ca7aff4c5db8bdbc82191432a389
Elias Ramirez <eliasramirez1999@gmail.com>
2020-11-26 23:48:43 -0800

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -2,15 +2,16 @@
 
 fdisk /dev/sda
 
-mkfs.vfat -F 32 /dev/sda1
+mkfs.vfat -F32 /dev/sda1
 mkfs.ext4 /dev/sda2
 
 mount /dev/sda2 /mnt/
+
+mv arch-chroot.sh /mnt/
 
 pacstrap -i /mnt/ base base-devel
 
 genfstab -U -p /mnt >> /mnt/etc/fstab
 
-mv arch-chroot.sh /mnt/
 
-arch-chroot /mnt
+arch-chroot /mnt/
