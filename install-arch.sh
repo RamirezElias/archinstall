11e0ac593ce571685d59efe94d91adf2d6e12db2
Elias Ramirez <eliasramirez1999@gmail.com>
2020-11-26 22:07:35 -0800

    Update install-arch.sh

--- install-arch.sh
+++ install-arch.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
+fdsik /dev/sda
 
-$ sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << FDISK_CMDS  | sudo fdisk /dev/sda
-g      # create new GPT partition
-n      # add new partition
-1      # partition number
-       # default - first sector
-+550MiB # partition size
-n      # add new partition
-2      # partition number
-       # default - first sector
-       # default - last sector
-t      # change partition type
-1      # partition number
-1     # Linux filesystem
-w      # write partition table and exit
-FDISK_CMDS
 mkfs.vfat -F 32 /dev/sda1
 mkfs.ext4 /dev/sda2
 
